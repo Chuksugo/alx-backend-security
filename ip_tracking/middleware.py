b353c53a6fc35705da21cfa3865f73edfb465ba4
ugochukwuemeka <chukwuemekaugochukwu00@gmail.com>
2025-09-06 12:49:52 +0100

    Implement Task 1: IP Blacklisting with BlockedIP model, middleware check, and block_ip command

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,19 +1,24 @@
-from .models import RequestLog
+from django.http import HttpResponseForbidden
 from django.utils.timezone import now
+from .models import RequestLog, BlockedIP
+
 
 class IPLoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Get IP address
         ip_address = request.META.get('REMOTE_ADDR')
+
+        # Check if IP is blocked
+        if BlockedIP.objects.filter(ip_address=ip_address).exists():
+            return HttpResponseForbidden("Your IP is blocked.")
+
         # Log request
         RequestLog.objects.create(
             ip_address=ip_address,
             timestamp=now(),
             path=request.path
         )
-        # Continue with response
-        response = self.get_response(request)
-        return response
+
+        return self.get_response(request)
